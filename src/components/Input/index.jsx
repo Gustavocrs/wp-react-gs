e4bc9d2e7fae3ccb43f1d4b86a5c1b7c7a3a5f19
Gustavocrs <86523009+Gustavocrs@users.noreply.github.com>
2022-09-25 17:11:01 -0300

    webpack-gustavocrs

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const SInput = styled.input`
+  width: 300px;
+  height: 25px;
+  background-color: red;
+`;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "./src/index.jsx"),
+  output: {
+    filename: "build.js",
+    path: path.resolve(__dirname, "static"),
+  },
+  module: {
+    rules: [
+      {
+        //Jsx & Js
+        test: /\.(js|jsx)$/,
+        exclude: /node-modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        //Svgs
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+      {
+        //Imagens
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    hot: true,
+    port: 3333,
+  },
+};

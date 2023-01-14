36fc2622a6d7c1c47566f720d7d2a852cdf9a1d6
Gustavocrs <86523009+Gustavocrs@users.noreply.github.com>
2023-01-14 20:26:14 -0300

    Inclusão de console de erros

--- src/wrapperAPI/index.jsx
+++ src/wrapperAPI/index.jsx
@@ -3,44 +3,56 @@ import axios from "axios";
 export const getApi = async (url, config) => {
   let data = "";
   if (config) {
-    await axios.get(url).then((response) => {
-      data = response.data;
-      console.log("RESPOSTA GET COM CONFIG");
-      console.log(data);
-      console.log("Configurações GET");
-      console.log(config);
-    });
+    await axios
+      .get(url)
+      .then((response) => {
+        data = response.data;
+        console.log("RESPOSTA GET COM CONFIG");
+        console.log(data);
+        console.log("Configurações GET");
+        console.log(config);
+      })
+      .catch((error) => console.log(error));
   } else {
-    await axios.get(url).then((response) => {
-      data = response.data;
-      console.log("RESPOSTA GET SEM CONFIG");
-      console.log(data);
-    });
+    await axios
+      .get(url)
+      .then((response) => {
+        data = response.data;
+        console.log("RESPOSTA GET SEM CONFIG");
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
   }
 };
 
-export const postApi = async (url, config) => {
+export const postApi = (url, config) => {
   let data = "";
   if (config) {
-    await axios.post(url, config).then((response) => {
-      data = response.data;
-      console.log("RESPOSTA POST");
-      console.log(data);
-    });
+    axios
+      .post(url, config)
+      .then((response) => {
+        data = response.data;
+        console.log("RESPOSTA POST");
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
   } else {
     console.log("VOCÊ DEVE PASSAR UM ARRAY COM OS DADOS DO BODY");
   }
 };
 
-export const deleteApi = async (url, config, id) => {
+export const deleteApi = (url, config, id) => {
   let data = "";
   if (id) {
     let newUrl = url + id;
-    await axios.delete(newUrl, config).then((response) => {
-      data = response.data;
-      console.log("RESPOSTA DELETE");
-      console.log(data);
-    });
+    axios
+      .delete(newUrl, config)
+      .then((response) => {
+        data = response.data;
+        console.log("RESPOSTA DELETE");
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
   } else {
     console.log("VOCÊ DEVE PASSAR UM ARRAY COM OS DADOS DO BODY");
   }

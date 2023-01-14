c459cafe061929140cfff39e67dbb088f1e8b464
Gustavocrs <86523009+Gustavocrs@users.noreply.github.com>
2023-01-14 18:49:26 -0300

    Criação de wrapperAPI (GET, POST e DELETE).

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "./components/Container/index.jsx";
 import { GlobalStyle } from "./components/GlobalStyle/index.jsx";
+
+import { getApi, postApi, deleteApi } from "./components/wrapperAPI/index.jsx";
+
 const App = () => {
+  let baseUrl = "https://jsonplaceholder.typicode.com/posts?_limit=3";
+  let baseUrlDelete = "https://jsonplaceholder.typicode.com/posts/";
+  let conf = [
+    {
+      headers: {
+        "Content-type": "application/json",
+      },
+    },
+  ];
+  let confPost = [
+    {
+      headers: {
+        "Content-type": "application/json",
+      },
+    },
+    {
+      data: {
+        title: "Teste Title",
+        body: "Teste Body",
+        userId: 1,
+      },
+    },
+  ];
+  useEffect(() => {
+    getApi(baseUrl);
+    postApi(baseUrl, confPost);
+  }, []);
+
   return (
     <Container>
       <GlobalStyle />
       <h1>New Webpack App</h1>
+      <button onClick={() => deleteApi(baseUrlDelete, conf, 2)}>DELETE</button>
     </Container>
   );
 };
---
+++ src/components/wrapperAPI/index.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+export const getApi = async (url, config) => {
+  let data = "";
+  if (config) {
+    await axios.get(url).then((response) => {
+      data = response.data;
+      console.log("RESPOSTA GET COM CONFIG");
+      console.log(data);
+      console.log("Configurações GET");
+      console.log(config);
+    });
+  } else {
+    await axios.get(url).then((response) => {
+      data = response.data;
+      console.log("RESPOSTA GET SEM COM CONFIG");
+      console.log(data);
+    });
+  }
+};
+
+export const postApi = async (url, config) => {
+  let data = "";
+  if (config) {
+    await axios.post(url, config).then((response) => {
+      data = response.data;
+      console.log("RESPOSTA POST");
+      console.log(data);
+    });
+  } else {
+    console.log("VOCÊ DEVE PASSAR UM ARRAY COM OS DADOS DO BODY");
+  }
+};
+
+export const deleteApi = async (url, config, id) => {
+  let data = "";
+  if (id) {
+    let newUrl = url + id;
+    await axios.delete(newUrl, config).then((response) => {
+      data = response.data;
+      console.log("RESPOSTA DELETE");
+      console.log(data);
+    });
+  } else {
+    console.log("VOCÊ DEVE PASSAR UM ARRAY COM OS DADOS DO BODY");
+  }
+};

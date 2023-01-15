d0952ab0b63d35b9c1fdeb919cbb182a051c3f2c
Gustavocrs <86523009+Gustavocrs@users.noreply.github.com>
2023-01-15 15:52:46 -0300

    Criação de Hook useApi

--- src/App.jsx
+++ src/App.jsx
@@ -2,58 +2,68 @@ import React, { useEffect, useState } from "react";
 import { Container } from "./components/Container/index.jsx";
 import { GlobalStyle } from "./components/GlobalStyle/index.jsx";
 
-import { getApi, postApi, deleteApi } from "./wrapperAPI/index.jsx";
+import useApi from "./hooks/useApi.jsx";
 
 const App = () => {
-  let baseUrl1 = "https://jsonplaceholder.typicode.com/posts?_limit=3";
-  let baseUrlDelete = "https://jsonplaceholder.typicode.com/posts/";
-  let conf = [
-    {
-      headers: {
-        "Content-type": "application/json",
-      },
-    },
-  ];
-  let confPost1 = [
-    {
-      headers: {
-        "Content-type": "application/json",
-      },
-    },
-    {
-      data: {
-        title: "Teste Title",
-        body: "Teste Body",
-        userId: 1,
-      },
-    },
-  ];
+  const { response, loading, error } = useApi({
+    method: "get",
+    baseUrl: "https://jsonplaceholder.typicode.com/posts/",
+    url: "?_limit=3",
+  });
 
-  const hundlePost = (baseUrl, confPost) => {
-    postApi(baseUrl, confPost);
-  };
-  const hundleDelete = (baseUrlDelete, conf, id) => {
-    deleteApi(baseUrlDelete, conf, id);
-  };
-  const hundleGet = (baseUrl, confPost) => {
-    let api = getApi(baseUrl, confPost);
-    console.log(api);
-  };
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (response !== null) {
+      setData(response);
+    }
+  }, [response]);
 
   return (
     <Container>
       <GlobalStyle />
       <h1>New Webpack App</h1>
-      <button onClick={() => hundleGet(baseUrl1, confPost1)}>GET</button>
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        <ul>{data && data.map((e) => <li key={e.id}>{e.title}</li>)}</ul>
+      )}
       <div>
         <ul></ul>
       </div>
-      {/* <button onClick={() => hundleDelete(baseUrlDelete, conf, 2)}>
-        DELETE
-      </button>
-      <button onClick={() => hundlePost(baseUrl, confPost)}>POST</button> */}
     </Container>
   );
 };
 
 export default App;
+
+// let conf = [
+//   {
+//     headers: {
+//       "Content-type": "application/json",
+//     },
+//   },
+// ];
+
+// let baseUrlDelete = "https://jsonplaceholder.typicode.com/posts/";
+// let confPost1 = [
+//   {
+//     headers: {
+//       "Content-type": "application/json",
+//     },
+//   },
+//   {
+//     data: {
+//       title: "Teste Title",
+//       body: "Teste Body",
+//       userId: 1,
+//     },
+//   },
+// ];
+
+// const hundlePost = (baseUrl, confPost) => {
+//   postApi(baseUrl, confPost);
+// };
+// const hundleDelete = (baseUrlDelete, conf, id) => {
+//   deleteApi(baseUrlDelete, conf, id);
+// };
---
+++ src/hooks/useApi.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useApiGet = ({
+  method,
+  baseUrl = null,
+  url,
+  body = null,
+  headers = null,
+}) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const getApi = () => {
+    axios[method](baseUrl + url, JSON.parse(headers, JSON.parse(body)))
+      .then((res) => {
+        setResponse(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getApi();
+  }, [baseUrl, url, body, headers]);
+
+  return { response, error, loading };
+};
+
+export default useApiGet;
+
+// export const postApi = (url, config) => {
+//   let data = "";
+//   if (config) {
+//     axios
+//       .post(url, config)
+//       .then((response) => {
+//         data = response.data;
+//         console.log("RESPOSTA POST");
+//         console.log(data);
+//         return data;
+//       })
+//       .catch((error) => console.log(error));
+//   } else {
+//     console.log("VOCÊ DEVE PASSAR UM ARRAY COM OS DADOS DO BODY");
+//   }
+// };
+
+// export const deleteApi = (url, config, id) => {
+//   let data = "";
+//   if (id) {
+//     let newUrl = url + id;
+//     axios
+//       .delete(newUrl, config)
+//       .then((response) => {
+//         data = response.data;
+//         console.log("RESPOSTA DELETE");
+//         console.log(data);
+//         return data;
+//       })
+//       .catch((error) => console.log(error));
+//   } else {
+//     console.log("VOCÊ DEVE PASSAR UM ARRAY COM OS DADOS DO BODY");
+//   }
+// };
